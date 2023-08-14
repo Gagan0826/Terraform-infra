3c274da51f72702473b63cec238eeec41fb12923
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-15 02:41:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terr-backend-1"
+    bucket         = "terr-bucket-backend-1"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "MyLockTable"  
