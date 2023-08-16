8f5e0366032bfb82d1470e177a103d04a45e283c
Gagan <gaganbrgirish26@gmail.com>
2023-08-16 10:37:51 +0530

    rds password config

--- rds.tf
+++ rds.tf
@@ -7,11 +7,18 @@
   storage_type         = var.storage_type
   engine_version       = var.engine_version
   instance_class       = var.instance_class
-  username             = var.db_username
-  password             = var.db_password
+  username             = jsondecode(data.aws_secretsmanager_secret_version.credentials.secret_string).username
+  password             = jsondecode(data.aws_secretsmanager_secret_version.credentials.secret_string).password
   port                 = var.db_port
   publicly_accessible = false
   skip_final_snapshot= true
   vpc_security_group_ids = [aws_security_group.rds_private_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.private_db_subnet_group.name
 }
+
+data "aws_secretsmanager_secret_version" "credentials"{
+  secret_id ="week-1-rds"
+}
+
+
+
---
+++ variables.tfvars
@@ -0,0 +1,11 @@
+instance_type       = "t2.micro"
+ami                 = "ami-0f5ee92e2d63afc18"
+key_name            = "w1"
+
+db_name             = "Employee_db"
+engine              = "mysql"
+allocated_storage   = 20
+storage_type        = "gp2"
+engine_version      = "8.0.33"
+instance_class      = "db.t2.micro"
+db_port             = 3306
